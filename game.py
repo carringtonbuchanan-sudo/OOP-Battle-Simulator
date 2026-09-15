029d0764c8d87e8b60b5657d28c2b3cb2d734f3a
carringtonbuchanan-sudo <carrington.buchanan@ascte.org>
2026-09-15 09:08:18 -0500

    Battle Loop Created w/ Round #s

--- game.py
+++ game.py
@@ -4,6 +4,25 @@ from hero import Hero
 
 ARENA_NAME = "The Diagonal Flame"
 
+def battle(hero : Hero, enemy : Goblin):
+    round_num = 1
+    while hero.is_alive() and enemy.is_alive():
+        print(f"Round {round_num}")
+        round_num += 1
+        
+        hero_damage = hero.attack()
+        enemy.take_damage(hero_damage)
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
+        print()
+
+    if hero.is_alive():
+       print(f"{hero.name} wins!")
+    else:
+        print(f"{hero.name} has been defeated. {enemy.name} wins.")
+    
+
 
 def main():
     """Open the arena and introduce its first opponent."""
@@ -24,15 +43,13 @@ def main():
     hero = Hero("Chast")
     print(f"But wait! Here comes our hero now. {hero.name} enters the arena with {hero.health} health.")
     hero.battle_cry("Chast")
-    
-    print()
-
     print(f"Let the battle begin.")
     print()
 
-    heroDamage = hero.attack()
-    goblin.take_damage(heroDamage)
-    print(f"{hero.name} attacks {goblin.name} with {heroDamage} power! {goblin.name}'s health is now {goblin.health}.")
+    battle(hero, goblin)
+
+    
+    
 
 
 if __name__ == "__main__":
--- hero.py
+++ hero.py
@@ -2,15 +2,17 @@ import random
 
 class Hero:
     def __init__(self, name):
-        self.name = name
-        self.health = 100
-        self.attack_power = 20
+        self.name = name #each hero created has different name
+        self.health = 100 #each hero begins with 100 health
+        self.attack_power = 20 #each hero can max at 20 damage
 
     def attack(self):
         return random.randint(1, self.attack_power)
 
     def take_damage(self, damage):
-        self.health = max(0, self.health - damage)
+        self.health = self.health - damage
+        if self.health < 0:
+            self.health = 0
         print(f"{self.name} takes {damage} damage. Health: {self.health}")
 
     def is_alive(self):
