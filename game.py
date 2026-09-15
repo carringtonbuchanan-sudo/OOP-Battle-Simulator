ed85f4529d0db7a8d49ae90ebf704b1bf40dddac
carringtonbuchanan-sudo <carrington.buchanan@ascte.org>
2026-09-14 20:07:10 -0500

    Build Hero Class

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from goblin import Goblin
+from hero import Hero
 
 
 ARENA_NAME = "The Diagonal Flame"
@@ -15,8 +16,23 @@ def main():
 
     goblinTwo = Goblin("Ezra")
     print(f"{goblinTwo.name} enters the arena with {goblinTwo.health} health.")
+    print()
 
     print("But no hero has answered the call... yet.")
+    print()
+
+    hero = Hero("Chast")
+    print(f"But wait! Here comes our hero now. {hero.name} enters the arena with {hero.health} health.")
+    hero.battle_cry("Chast")
+    
+    print()
+
+    print(f"Let the battle begin.")
+    print()
+
+    heroDamage = hero.attack()
+    goblin.take_damage(heroDamage)
+    print(f"{hero.name} attacks {goblin.name} with {heroDamage} power! {goblin.name}'s health is now {goblin.health}.")
 
 
 if __name__ == "__main__":
